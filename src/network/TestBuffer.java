a61ddee18e34eb169d8991eace8f78507c3d3f76
penglei <yupengniaosanshi@gmail.com>
2017-06-14 09:33:24 +0800

    save

--- src/network/TestBuffer.java
+++ src/network/TestBuffer.java
@@ -1,6 +1,8 @@
 package network;
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.CharBuffer;
 import java.util.Arrays;
 
 /**
@@ -9,7 +11,42 @@ import java.util.Arrays;
 public class TestBuffer {
     public static void main(String[] args) {
 //        test1();
-        test2();
+//        test2();
+//        endians();
+        testMark();
+    }
+
+    private static void testMark() {
+        ByteBuffer byteBuffer = ByteBuffer.wrap("abcdef".getBytes());
+        System.out.println(byteBuffer.get());
+        System.out.println(byteBuffer.get());
+        System.out.println(byteBuffer.position());
+        byteBuffer.mark();
+        System.out.println(byteBuffer.get());
+        System.out.println(byteBuffer.get());
+        System.out.println(byteBuffer.get());
+        System.out.println(byteBuffer.position());
+        byteBuffer.reset();
+        System.out.println(byteBuffer.position());
+        System.out.println(byteBuffer.get(0));
+        System.out.println(byteBuffer.position());
+    }
+
+    private static void endians() {
+        String s = "abcdef";
+        ByteBuffer byteBuffer = ByteBuffer.allocate(12);
+        byteBuffer.asCharBuffer().put(s);
+        System.out.println(Arrays.toString(byteBuffer.array()));
+
+        byteBuffer.clear();
+        byteBuffer.order(ByteOrder.BIG_ENDIAN);
+        byteBuffer.asCharBuffer().put(s);
+        System.out.println(Arrays.toString(byteBuffer.array()));
+
+        byteBuffer.clear();
+        byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.asCharBuffer().put(s);
+        System.out.println(Arrays.toString(byteBuffer.array()));
     }
 
     private static void test2() {
